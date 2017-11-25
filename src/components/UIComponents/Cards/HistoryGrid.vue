855a4b9beb289465e996d929a3366b906db2f84e
littleguga <fed777os@gmail.com>
2017-11-25 22:31:15 +0300

    working on HistoryGrid draft

--- src/components/UIComponents/Cards/HistoryGrid.vue
+++ src/components/UIComponents/Cards/HistoryGrid.vue
@@ -2,53 +2,15 @@
   <div class="row">
     <div class="col-md-12">
       <div class="card">
-        <paper-table :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
+        <paper-table :title="title" :sub-title="subTitle" :data="data" :columns="columns">
 
         </paper-table>
       </div>
     </div>
-
   </div>
 </template>
 <script>
   import PaperTable from 'components/UIComponents/PaperTable.vue'
-//  const tableColumns = ['#', 'Name', 'Summary', 'Loan %', 'Expected date', 'Expected Profit']
-//  const tableData = [{
-//    '#': 1,
-//    name: 'Dakota Rice',
-//    summary: '€ 1000',
-//    'loan %': '5',
-//    'expected date': '8 December',
-//    'expected profit': '€ 50'
-//  }, {
-//    '#': 2,
-//    name: 'Dakota Rice',
-//    summary: '€ 1000',
-//    'loan %': '5',
-//    'expected date': '8 December',
-//    'expected profit': '€ 50'
-//  }, {
-//    '#': 3,
-//    name: 'Dakota Rice',
-//    summary: '€ 1000',
-//    'loan %': '5',
-//    'expected date': '8 December',
-//    'expected profit': '€ 50'
-//  }, {
-//    '#': 4,
-//    name: 'Dakota Rice',
-//    summary: '€ 1000',
-//    'loan %': '5',
-//    'expected date': '8 December',
-//    'expected profit': '€ 50'
-//  }, {
-//    '#': 5,
-//    name: 'Dakota Rice',
-//    summary: '€ 1000',
-//    'loan %': '5',
-//    'expected date': '8 December',
-//    'expected profit': '€ 50'
-//  }]
 
   export default {
     name: 'history-grid',
@@ -70,9 +32,9 @@
       },
       columns: {
         type: Array,
-        default: ['#', 'Name', 'Summary', 'Loan %', 'Expected date', 'Expected Profit']
+        default: () => ['#', 'Name', 'Summary', 'Loan %', 'Expected date', 'Expected Profit']
       },
-      gridData: {
+      data: {
         type: Array,
         default: () => [
           {
@@ -92,8 +54,9 @@
        */
       initGrid () {
         // var gridIdQuery = `#${this.gridId}`
-        // @TODO: pass gridData to real grid
-        console.log(this.gridData)
+        // console.log(this.data)
+        // console.log(this.title)
+        // console.log(this.subTitle)
       },
       /***
        * Assigns a random id to the grid
