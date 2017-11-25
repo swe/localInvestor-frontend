6c539cbc098bba8b2958b6d1b3bffa64d0dab64b
littleguga <fed777os@gmail.com>
2017-11-26 02:24:48 +0300

    add draft of Explore grid

--- src/components/Dashboard/Views/Explore.vue
+++ src/components/Dashboard/Views/Explore.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="row">
+  <div class="row">
 
-      <div class="col-md-12">
-        <div class="card card-plain">
-          <paper-table type="hover" :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
+    <div class="col-md-12">
+      <div class="card card-plain">
+        <paper-table type="hover" :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
 
-          </paper-table>
-        </div>
+        </paper-table>
       </div>
-
     </div>
+
+  </div>
 </template>
 <script>
   import PaperTable from 'components/UIComponents/PaperTable.vue'
   const tableColumns = ['No', 'Name', 'Location', 'Risks', 'Return']
-  const tableData = [{
-    no: 1,
-    name: 'Hewlett Packard Enterprise',
-    location: 'Santa Clara, CA, USA',
-    risks: 'Low',
-    return: '1 month'
-  },
-  {
-    no: 2,
-    name: 'Nordea Bank AB',
-    location: 'Stockholm, Sweden',
-    risks: 'Medium',
-    return: '2 months'
-  }]
+  const tableData = [
+    {
+      no: 1,
+      name: 'Hewlett Packard Enterprise',
+      location: 'Santa Clara, CA, USA',
+      risks: 'Low',
+      return: '1 month'
+    },
+    {
+      no: 2,
+      name: 'Nordea Bank AB',
+      location: 'Stockholm, Sweden',
+      risks: 'Medium',
+      return: '2 months'
+    }
+  ]
 
   export default {
     components: {
