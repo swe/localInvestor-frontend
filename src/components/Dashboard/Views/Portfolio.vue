98cc48019f48ce53600e27e30fcaff3970af3e6c
littleguga <fed777os@gmail.com>
2017-11-25 09:39:22 +0300

    working on historyGrid draft

--- src/components/Dashboard/Views/Portfolio.vue
+++ src/components/Dashboard/Views/Portfolio.vue
@@ -8,49 +8,41 @@
         </div>
       </div>
 
-      <div class="col-md-12">
-        <div class="card card-plain">
-          <paper-table type="hover" :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
-
-          </paper-table>
-        </div>
-      </div>
-
     </div>
 </template>
 <script>
   import PaperTable from 'components/UIComponents/PaperTable.vue'
-  const tableColumns = ['Id', 'Name', 'Salary', 'Country', 'City']
+  const tableColumns = ['N', 'Name', 'Summary', 'Loan %', 'Expected Date', 'Expected Profit']
   const tableData = [{
-    id: 1,
+    n: 1,
     name: 'Dakota Rice',
-    salary: '$36.738',
-    country: 'Niger',
-    city: 'Oud-Turnhout'
+    summary: '$36.738',
+    loan: 'Niger',
+    expected_date: 'Oud-Turnhout'
   },
   {
-    id: 2,
+    n: 2,
     name: 'Minerva Hooper',
     salary: '$23,789',
     country: 'Curaçao',
     city: 'Sinaai-Waas'
   },
   {
-    id: 3,
+    n: 3,
     name: 'Sage Rodriguez',
     salary: '$56,142',
     country: 'Netherlands',
     city: 'Baileux'
   },
   {
-    id: 4,
+    n: 4,
     name: 'Philip Chaney',
     salary: '$38,735',
     country: 'Korea, South',
     city: 'Overland Park'
   },
   {
-    id: 5,
+    n: 5,
     name: 'Doris Greene',
     salary: '$63,542',
     country: 'Malawi',
@@ -64,14 +56,8 @@
     data () {
       return {
         table1: {
-          title: 'Stripped Table',
-          subTitle: 'Here is a subtitle for this table',
-          columns: [...tableColumns],
-          data: [...tableData]
-        },
-        table2: {
-          title: 'Table on Plain Background',
-          subTitle: 'Here is a subtitle for this table',
+          title: 'Loan Responses Pack',
+          subTitle: 'You loaned this companies at 8 November',
           columns: [...tableColumns],
           data: [...tableData]
         }
